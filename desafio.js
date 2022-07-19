f9c89c50d877edcee4eee5c65e3d361d54589416
PabloPaura <pablodpaura@gmail.com>
2022-07-19 16:53:28 -0300

    Creé un ciclo

---
+++ desafio.js
@@ -0,0 +1,16 @@
+let = nombre = prompt("Ingresá tu nombre");
+    alert("Hola " + nombre + ". Pulsá aceptar para ingresar un número");
+
+    console.log(nombre);
+
+let = numTabla = parseInt(prompt("Ingresá el número del cual querés saber la tabla"));
+    console.log("Querés sabel la tabla del " + numTabla);
+
+    for(let  i = 1; i <= 10; i++){
+    let = tabla = numTabla *i;
+    console.log(`${numTabla} x ${i} = ${tabla}`);
+    document.write(`${numTabla} x ${i} = ${tabla}  <br>`);
+}
+alert("Gracias por participar " + nombre);
+
+
