e8908f3f0f7d869a67b3f97098c7747fbd84cef8
PabloPaura <pablodpaura@gmail.com>
2022-08-06 20:34:48 -0300

    Agrego plantilla para ingresar estudiante

--- proyecto.js
+++ proyecto.js
@@ -3,22 +3,23 @@
 
 // //Función constructora:
 class Estudiante{
-    constructor(nombre, apellido, edad, materiaFavorita){
+    constructor(nombre, apellido, edad, materiaFavorita, imagen){
         this.nombre = nombre,
         this.apellido = apellido,
         this.edad = edad,
         this.materiaFavorita = materiaFavorita
+        this.imagen = imagen
     }
-    }
+}
 
 
 // //Instanciación de objetos:
-const estudiante1 = new Estudiante("Pablo", "Paura", 48, "Matemática")
-const estudiante2 = new Estudiante("Lorena", "Suarez", 41, "Lengua")
-const estudiante3 = new Estudiante("Adrián", "Gonzalez", 36, "Naturales")
-const estudiante4 = new Estudiante("Lucía", "Mendez", 44, "Música")
-const estudiante5 = new Estudiante("Marcelo", "Pérez", 29, "Química")
-const estudiante6 = new Estudiante("Mariela", "Gonzalez", 40, "Música")
+const estudiante1 = new Estudiante("Pablo", "Paura", 48, "Matemática", "assets/avatar_1.jpg")
+const estudiante2 = new Estudiante("Lorena", "Suarez", 41, "Lengua", "assets/avatar_4.jpg")
+const estudiante3 = new Estudiante("Adrián", "Gonzalez", 36, "Naturales", "assets/avatar_2.jpg")
+const estudiante4 = new Estudiante("Lucía", "Mendez", 44, "Música", "assets/avatar_5.jpg")
+const estudiante5 = new Estudiante("Marcelo", "Pérez", 29, "Química", "assets/avatar_3.jpg")
+const estudiante6 = new Estudiante("Mariela", "Gonzalez", 40, "Música", "assets/avatar_6.jpg")
 
 // //Control de la función:
 // // console.log(estudiante1)
@@ -29,16 +30,6 @@ const estudiante6 = new Estudiante("Mariela", "Gonzalez", 40, "Música")
 const registro = [estudiante1, estudiante2, estudiante3, estudiante4, estudiante5, estudiante6]
 // console.log(registro)
 
-// //Función para ingresar nuevo estudiante:
-function nuevoEstudiante(){
-    let nombreIngresado = prompt("Ingresá tu nombre")
-    let apellidoIngresado = prompt("Ingresá tu apellido")
-    let edadIngresada = prompt("Ingresá tu edad")
-    let materiaFavoritaIngresada = prompt("Ingresá tu materia favorita")
-    let estudianteCreado = new Estudiante(nombreIngresado, apellidoIngresado, edadIngresada, materiaFavoritaIngresada)
-    registro.push(estudianteCreado)
-    console.log(estudianteCreado)
-}
 
 //Funciones:
 
@@ -50,7 +41,8 @@ function nuevoEstudiante(){
     let materiaFavoritaIngresada = prompt("Ingresá tu materia favorita")
     let estudianteCreado = new Estudiante(nombreIngresado, apellidoIngresado, edadIngresada, materiaFavoritaIngresada)
     registro.push(estudianteCreado)
-    console.log(estudianteCreado)
+    // 
+
 }
 
 //Funcion para iniciar el programa de opciones:
@@ -66,6 +58,27 @@ function iniciar(){
                         `))
     menu(opcion)
 }
+
+//Plantilla para crear nuevo estudiante:
+
+let divEstudiantes = document.getElementById("alumnosCards")
+divEstudiantes.setAttribute("class", "card")
+registro.forEach((estudiante)=>{
+    let nuevoCard = document.createElement("div")
+    nuevoCard.innerHTML = `<article id="${estudiante.id}class="card">
+                                <h3 class="nombreCard">${estudiante.nombre}  ${estudiante.apellido}</h3>
+                                <img src="${estudiante.imagen}" alt="">
+                                <div class="content">
+                                    <p class="edadCard">Edad: ${estudiante.edad}</p>
+                                    <p class="materiaCard">Le gusta mucho: ${estudiante.materiaFavorita}</p>
+                                    <a href="" target="blank">Contactar</a>
+                                </div>
+    
+                            </article>`
+    divEstudiantes.appendChild(nuevoCard)
+
+})
+
 
 //Función para buscar un amigo:
 function buscarAmigo(){
